f618e6a67d4f4e93e28e6b04a18531567ed6d7ec
Tuo Chen (Beyondsoft Corporation) <v-tuchen@microsoft.com>
2017-06-05 19:49:48 -0700

    Add new android project

---
+++ XamarinAndroidPro2/XamarinAndroidPro2/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace XamarinAndroidPro2
+{
+    [Activity(Label = "XamarinAndroidPro2", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            MobileCenter.Start("5e27edb7-974f-46bc-baa2-a16d62a33644",
+                   typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
+
---
+++ XamarinAndroidPro2/XamarinAndroidPro2/obj/Debug/android/src/md559f5b251de6a9e8c4a7a565e74006f54/MainActivity.java
@@ -0,0 +1,47 @@
+package md559f5b251de6a9e8c4a7a565e74006f54;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("XamarinAndroidPro2.MainActivity, XamarinAndroidPro2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("XamarinAndroidPro2.MainActivity, XamarinAndroidPro2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
